bery-test/vendor_awaken

vendor: tools: Refactor our json script
* Also update build information color.

Change-Id: Idfc991be192f4a9e3c39e098cb4ff2e61444812f
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

build/tasks/bacon.mk
@@ -20,19 +20,18 @@ AWAKEN_TARGET_PACKAGE := $(PRODUCT_OUT)/awaken-$(AWAKEN_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_CYN="\033[36m"
 CL_PRP="\033[35m"
-CL_RED="\033[31m"
-CL_GRN="\033[32m"
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(AWAKEN_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(AWAKEN_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(AWAKEN_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/awaken/tools/generate_json_build_info.sh $(AWAKEN_TARGET_PACKAGE)
-	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
-	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(AWAKEN_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(AWAKEN_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(AWAKEN_TARGET_PACKAGE)"${CL_RST}
-	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+	echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_CYN}
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(AWAKEN_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(AWAKEN_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(AWAKEN_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}

tools/generate_json_build_info.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 CL_RED="\033[31m"
-CL_GRN="\033[1;32m"
-CL_BLU="\033[34m"
 CL_CYN="\033[1;36m"
-CL_RST="\033[0m"
-CL_YLW="\033[1;33m"
+CL_PRP="\033[35m"
 CL_NC="\033[0m"
 if [ "$1" ]; then
     echo "Generating .json"
@@ -15,13 +12,13 @@ if [ "$1" ]; then
         # only generate for official builds. unless forced with 'export FORCE_JSON=1'
         if [[ $file_name == *"official"* ]] || [[ $FORCE_JSON == 1 ]]; then
             if [[ $FORCE_JSON == 1 ]]; then
-                echo -e "${CL_GRN}Forced generation of json${CL_NC}"
+                echo -e "${CL_CYN}Forced generation of json${CL_NC}"
             fi
             file_size=$(stat -c%s $file_path)
-            md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+            md5=$(md5sum $file_path | awk '{ print $1 }');
             datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-            id=$(sha256sum $file_path | awk '{ print $1 }');
-            build_type=$(grep ro\.awaken\.build\.type ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            id=$(cat "$file_path.sha256sum" | cut -d' ' -f1);
+            build_type=$(grep ro\.awaken\.buildtype ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             base_version=$(grep ro\.awaken\.base\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             link="https://sourceforge.net/projects/project-awaken/files/${DEVICE}/${file_name}/download"
             echo "{" > $file_path.json
@@ -38,10 +35,10 @@ if [ "$1" ]; then
             echo "    }" >> $file_path.json
             echo "  ]" >> $file_path.json
             echo "}" >> $file_path.json
-            mv "${file_path}.json" "./out/target/product/$DEVICE/${DEVICE}.json"
-            echo -e "${CL_GRN}Done generating ${CL_RED}${DEVICE}.json${CL_NC}"
+            mv "${file_path}.json" "./${DEVICE}.json"
+            echo -e "${CL_CYN}Done generating ${CL_PRP}${DEVICE}.json${CL_NC}"
         else
-            echo -e "${CL_YLW}Skipped generating json for a non-official build${CL_NC}"
+            echo -e "${CL_RED}Skipped generating json for a non-official build${CL_NC}"
         fi
     fi
 fi

ProjectEverest-staging/vendor_everest

build/tasks/bacon.mk
@@ -20,19 +20,18 @@ EVEREST_TARGET_PACKAGE := $(PRODUCT_OUT)/Everest-$(EVEREST_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_CYN="\033[36m"
 CL_PRP="\033[35m"
-CL_RED="\033[31m"
-CL_GRN="\033[32m"
 
 .PHONY: bacon
 bacon: $(DEFAULT_GOAL) $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(EVEREST_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(EVEREST_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(EVEREST_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/everest/tools/generate_json_build_info.sh $(EVEREST_TARGET_PACKAGE)
-	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
-	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(EVEREST_TARGET_PACKAGE)${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(EVEREST_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(EVEREST_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"ID:"${CL_RED}" `md5sum $(EVEREST_TARGET_PACKAGE) | cut -d ' ' -f 1`"${CL_RST}
-	echo -e ${CL_BLD}${CL_GRN}"Path:"${CL_RED}" $(EVEREST_TARGET_PACKAGE)"${CL_RST}
-	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
+	echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_CYN}
+	echo -e ${CL_BLD}${CL_CYN}"Datetime :"${CL_PRP}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_PRP}" `du -sh $(EVEREST_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filehash: "${CL_PRP}" `md5sum $(EVEREST_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"Filename: "${CL_PRP} $(EVEREST_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"ID: "${CL_PRP}" `cat $(EVEREST_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_CYN}

tools/generate_json_build_info.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 CL_RED="\033[31m"
-CL_GRN="\033[1;32m"
-CL_BLU="\033[34m"
 CL_CYN="\033[1;36m"
-CL_RST="\033[0m"
-CL_YLW="\033[1;33m"
+CL_PRP="\033[35m"
 CL_NC="\033[0m"
 if [ "$1" ]; then
     echo "Generating .json"
@@ -15,13 +12,13 @@ if [ "$1" ]; then
         # only generate for OFFICIAL builds. unless forced with 'export FORCE_JSON=1'
         if [[ $file_name == *"OFFICIAL"* ]] || [[ $FORCE_JSON == 1 ]]; then
             if [[ $FORCE_JSON == 1 ]]; then
-                echo -e "${CL_GRN}Forced generation of json${CL_NC}"
+                echo -e "${CL_CYN}Forced generation of json${CL_NC}"
             fi
             file_size=$(stat -c%s $file_path)
-            md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+            md5=$(md5sum $file_path | awk '{ print $1 }');
             datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-            id=$(sha256sum $file_path | awk '{ print $1 }');
-            build_type=$(grep ro\.everest\.build\.type ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            id=$(cat "$file_path.sha256sum" | cut -d' ' -f1);
+            build_type=$(grep ro\.everest\.buildtype ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             base_version=$(grep ro\.everest\.base\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             link="https://sourceforge.net/projects/everestos-releases/files/${DEVICE}/${file_name}/download"
             echo "{" > $file_path.json
@@ -38,10 +35,10 @@ if [ "$1" ]; then
             echo "    }" >> $file_path.json
             echo "  ]" >> $file_path.json
             echo "}" >> $file_path.json
-            mv "${file_path}.json" "./out/target/product/$DEVICE/${DEVICE}.json"
-            echo -e "${CL_GRN}Done generating ${CL_RED}${DEVICE}.json${CL_NC}"
+            mv "${file_path}.json" "./${DEVICE}.json"
+            echo -e "${CL_CYN}Done generating ${CL_PRP}${DEVICE}.json${CL_NC}"
         else
-            echo -e "${CL_YLW}Skipped generating json for a non-official build${CL_NC}"
+            echo -e "${CL_RED}Skipped generating json for a non-official build${CL_NC}"
         fi
     fi
 fi